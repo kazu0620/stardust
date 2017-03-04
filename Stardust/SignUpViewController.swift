b1105e70724b90594b3354c520a3f5f2ac87ba47
kaneko takuji <kaneko@sansan.com>
2017-03-04 15:20:35 +0900

    Controller に Outlet を追加

---
+++ Stardust/SignUpViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignUpViewController.swift
+//  Stardust
+//
+//  Created by kaneko takuji on 2017/03/04.
+//  Copyright © 2017年 Stardust. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+    
+    @IBOutlet weak var signUp: UIButton!
+    @IBOutlet weak var connectTwitter: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
